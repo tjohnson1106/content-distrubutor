7c36bae710d2c7a6f7cf8ccb30230715c9080d02
Thomas Johnson <tjohnson1106@gmail.com>
2018-10-26 19:31:14 -0400

    created status response module

--- apps/deliveries/lib/youtube/status.ex
+++ apps/deliveries/lib/youtube/status.ex
@@ -2,4 +2,30 @@ defmodule Deliveries.YouTube.Status do
   alias Deliveries.YouTube.Client, as: Client
 
   @status_url "https://www.googleapis.com/youtube/tjohnson1106/v1/assetSearch?type=sound_recording&ownershipRestriction=none"
+
+  def status_for(isrcs) when is_list(isrcs) do
+    case do_status_for(isrcs) do
+      {:ok, response} ->
+        case decode_response(response.body) do
+          %{"error" => error_messages} ->
+            {:error, decode_response(response.body)}
+
+          _ ->
+            {:ok, decode_response(response.body)}
+        end
+
+      {:error, response} ->
+        {:error, decode_response(response.body)}
+    end
+  end
+
+  defp do_status_for(isrcs) do
+    (@status_url <> "&isrcs=" <> Enum.join(isrcs, "%2C%20"))
+    |> Client.make_request(:get)
+  end
+
+  defp decode_response(body) do
+    {:ok, body} = Poison.decode(body)
+    body
+  end
 end
