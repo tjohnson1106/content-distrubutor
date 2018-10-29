375ce95b51834c354560b1126434845d23b9bfe9
Thomas Johnson <tjohnson1106@gmail.com>
2018-10-29 16:00:36 -0400

    created status custom  route

--- apps/ytsr_status/lib/ytsr_status_web/router.ex
+++ apps/ytsr_status/lib/ytsr_status_web/router.ex
@@ -2,25 +2,34 @@ defmodule YtsrStatusWeb.Router do
   use YtsrStatusWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", YtsrStatusWeb do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
   end
 
   # Other scopes may use custom stacks.
   # scope "/api", YtsrStatusWeb do
   #   pipe_through :api
   # end
+
+  scope "/api", YtsrStatus do
+    pipe_through(:api)
+
+    scope "/v1" do
+      get("/status", StatusController, :index)
+    end
+  end
 end
